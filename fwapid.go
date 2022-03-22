62f53375e067b74fa854063075ef2087305c5fce
Daniel-Constantin Mierla <miconda@gmail.com>
2022-03-22 12:42:26 +0100

    added option to cache rules to revoke on timer routine

--- fwapid.go
+++ fwapid.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"log"
@@ -13,33 +14,39 @@ import (
 	"path/filepath"
 	"runtime"
 	"strings"
+	"sync"
+	"time"
 )
 
 const fwapidVersion = "1.0.0"
 
 // CLIOptions - structure for command line options
 type CLIOptions struct {
-	domain      string
-	httpsrv     string
-	httpssrv    string
-	httpsusele  bool
-	httpspubkey string
-	httpsprvkey string
-	allowfile   string
-	logfile     string
-	version     bool
+	domain        string
+	httpsrv       string
+	httpssrv      string
+	httpsusele    bool
+	httpspubkey   string
+	httpsprvkey   string
+	allowfile     string
+	logfile       string
+	cacheexpire   int64
+	timerinterval int
+	version       bool
 }
 
 var cliops = CLIOptions{
-	domain:      "",
-	httpsrv:     "",
-	httpssrv:    "",
-	httpsusele:  false,
-	httpspubkey: "",
-	httpsprvkey: "",
-	allowfile:   "",
-	logfile:     "",
-	version:     false,
+	domain:        "",
+	httpsrv:       "",
+	httpssrv:      "",
+	httpsusele:    false,
+	httpspubkey:   "",
+	httpsprvkey:   "",
+	allowfile:     "",
+	logfile:       "",
+	cacheexpire:   0,
+	timerinterval: 120,
+	version:       false,
 }
 
 type AllowRules struct {
@@ -59,6 +66,100 @@ type RuleData struct {
 }
 
 var vAllowRules AllowRules = AllowRules{}
+
+type CacheItem struct {
+	Key      string
+	Command  string
+	OpAdd    string
+	OpDel    string
+	Policy   string
+	IPAddr   string
+	DPorts   string
+	ExpireAt int64
+}
+
+type CacheData struct {
+	stop chan struct{}
+
+	wg    sync.WaitGroup
+	mu    sync.RWMutex
+	items map[string]CacheItem
+}
+
+var localCacheData *CacheData = nil
+
+func newCacheData(timerInterval time.Duration) *CacheData {
+	cd := &CacheData{
+		items: make(map[string]CacheItem),
+		stop:  make(chan struct{}),
+	}
+
+	cd.wg.Add(1)
+	go func(cleanupInterval time.Duration) {
+		defer cd.wg.Done()
+		cd.timerLoop(timerInterval)
+	}(timerInterval)
+
+	return cd
+}
+
+func (cd *CacheData) timerLoop(interval time.Duration) {
+	t := time.NewTicker(interval)
+	defer t.Stop()
+
+	for {
+		select {
+		case <-cd.stop:
+			return
+		case <-t.C:
+			cd.mu.Lock()
+			for cid, cit := range cd.items {
+				if cit.ExpireAt <= time.Now().Unix() {
+					log.Printf("revoking %s for key: %s\n", cit.IPAddr, cit.Key)
+					runCmd(exec.Command(cit.Command, cit.OpDel, "-s", cit.IPAddr, "-p", "tcp", "-m", "multiport",
+						"--dports", cit.DPorts, "-j", cit.Policy))
+					delete(cd.items, cid)
+				}
+			}
+			cd.mu.Unlock()
+		}
+	}
+}
+
+func (cd *CacheData) stopTimer() {
+	close(cd.stop)
+	cd.wg.Wait()
+}
+
+func (cd *CacheData) set(cid string, cit CacheItem) {
+	cd.mu.Lock()
+	defer cd.mu.Unlock()
+
+	cd.items[cid] = cit
+}
+
+var (
+	errItemNotInCache = errors.New("item not in cache")
+)
+
+func (cd *CacheData) get(cid string) (CacheItem, error) {
+	cd.mu.RLock()
+	defer cd.mu.RUnlock()
+
+	cit, ok := cd.items[cid]
+	if !ok {
+		return CacheItem{}, errItemNotInCache
+	}
+
+	return cit, nil
+}
+
+func (cd *CacheData) rm(cid string) {
+	cd.mu.Lock()
+	defer cd.mu.Unlock()
+
+	delete(cd.items, cid)
+}
 
 func isAllowed(vAction string, vKey string) bool {
 	for i := 0; i < len(vAllowRules.Rules); i++ {
@@ -83,6 +184,15 @@ func getAllowedIndex(vAction string, vKey string) int {
 				}
 			}
 			return -1
+		}
+	}
+	return -1
+}
+
+func getKeyIndex(vKey string) int {
+	for i := 0; i < len(vAllowRules.Rules); i++ {
+		if vAllowRules.Rules[i].Key == vKey {
+			return i
 		}
 	}
 	return -1
@@ -188,6 +298,19 @@ func fwapidHandler(w http.ResponseWriter, r *http.Request) {
 		runCmd(exec.Command(vAllowRules.Command, vAllowRules.OpAdd, "-s", ipAddr, "-p", "tcp", "-m", "multiport",
 			"--dports", vAllowRules.Rules[idxAllow].DPorts, "-j", vAllowRules.Policy))
 		fmt.Fprintf(w, "{ \"action\": \"allow\", \"address\": \"%s\" }", ipAddr)
+		if localCacheData != nil {
+			var cItem CacheItem = CacheItem{
+				Key:      tURL[2],
+				Command:  vAllowRules.Command,
+				OpAdd:    vAllowRules.OpAdd,
+				OpDel:    vAllowRules.OpAdd,
+				Policy:   vAllowRules.Policy,
+				IPAddr:   ipAddr,
+				DPorts:   vAllowRules.Rules[idxAllow].DPorts,
+				ExpireAt: time.Now().Unix() + cliops.cacheexpire,
+			}
+			localCacheData.set(ipAddr, cItem)
+		}
 	case "revoke", "revokeip":
 		log.Printf("revoked %s from %s via URL: %s\n", ipAddr, srcIPAddr, sURL)
 		runCmd(exec.Command(vAllowRules.Command, vAllowRules.OpDel, "-s", ipAddr, "-p", "tcp", "-m", "multiport",
@@ -250,6 +373,10 @@ func init() {
 	flag.StringVar(&cliops.logfile, "log", cliops.logfile, "path to log file or stdout")
 	flag.BoolVar(&cliops.httpsusele, "use-letsencrypt", cliops.httpsusele,
 		"use local letsencrypt certificates (requires domain)")
+	flag.Int64Var(&cliops.cacheexpire, "cache-expire", cliops.cacheexpire,
+		"duration in seconds to expire items in the cache")
+	flag.IntVar(&cliops.timerinterval, "timer-interval", cliops.timerinterval,
+		"duration in seconds to run timer callback for cache expiration")
 	flag.BoolVar(&cliops.version, "version", cliops.version, "print version")
 }
 
@@ -283,6 +410,9 @@ func main() {
 		log.SetOutput(lf)
 	}
 
+	if cliops.cacheexpire > 0 {
+		localCacheData = newCacheData(time.Duration(cliops.timerinterval) * time.Second)
+	}
 	http.HandleFunc("/", fwapidHandler)
 	errchan := startHTTPServices()
 	select {
